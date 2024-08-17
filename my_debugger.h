a8d64b30504fa75a42153c17feb8e79472e3093c
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2024-08-17 21:08:54 +1000

    Task3: Change Class Logger to #define

--- my_debugger.h
+++ my_debugger.h
@@ -10,33 +10,61 @@ using namespace std; // I understand its not advisable.
 
 #define DEBUG_MODE // Debugger activated
 
-#ifdef DEBUG_MODE
-	class Logger
+class Logger
+{
+public:
+	template<typename... Args>
+	static void log(const Args&... args)
 	{
-	public:
-		template<typename... Args>
-		static void log(const Args&... args)
-		{
-			ofstream logfile(msDebugFileName, ios_base::app);
-			if (logfile.fail()) {
-				cerr << "Unable to open debug file!" << endl;
-				return;
-			}
-			// Use a C++17 unary right fold, see Chapter 22.
-			((logfile << args), ...);
-			logfile << endl;
-
-            ((cout << args), ...);
-			cout << endl;
+		ofstream logfile(msDebugFileName, ios_base::app);
+		if (logfile.fail()) {
+			cerr << "Unable to open debug file!" << endl;
+			return;
 		}
+		// Use a C++17 unary right fold, see Chapter 22.
+		((logfile << args), ...);
+		logfile << endl;
 
-	private:
-		// static const string msDebugFileName = "debugfile2.txt";
-		// static const string Logger::msDebugFileName = "debugfile2.txt";
-		static const string msDebugFileName;
-	};
+		((cout << args), ...);
+		cout << endl;
+	}
+
+private:
+	// static const string msDebugFileName = "debugfile2.txt";
+	// static const string Logger::msDebugFileName = "debugfile2.txt";
+	static const string msDebugFileName;
+};
 	
 	// const string Logger::msDebugFileName = "debugfile.txt";
+
+
+#ifdef DEBUG_MODE
+	// class Logger
+	// {
+	// public:
+	// 	template<typename... Args>
+	// 	static void log(const Args&... args)
+	// 	{
+	// 		ofstream logfile(msDebugFileName, ios_base::app);
+	// 		if (logfile.fail()) {
+	// 			cerr << "Unable to open debug file!" << endl;
+	// 			return;
+	// 		}
+	// 		// Use a C++17 unary right fold, see Chapter 22.
+	// 		((logfile << args), ...);
+	// 		logfile << endl;
+
+    //         ((cout << args), ...);
+	// 		cout << endl;
+	// 	}
+
+	// private:
+	// 	// static const string msDebugFileName = "debugfile2.txt";
+	// 	// static const string Logger::msDebugFileName = "debugfile2.txt";
+	// 	static const string msDebugFileName;
+	// };
+	
+	// // const string Logger::msDebugFileName = "debugfile.txt";
 
 	#define log(...) Logger::log(__func__, "(): ", __VA_ARGS__)
 #else
